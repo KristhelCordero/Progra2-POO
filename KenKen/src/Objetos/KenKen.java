948f6efe1feaa4ec9b5979270365e48a4a25a9bb
KendallPiedra <114965407+KendallPiedra@users.noreply.github.com>
2023-10-19 12:16:43 -0600

    gf

--- KenKen/src/Objetos/KenKen.java
+++ KenKen/src/Objetos/KenKen.java
@@ -16,5 +16,15 @@ public class KenKen {
     int dificultad;
     String rutaImagen; //ruta a la imagen del Ken Ken
     List<List<Integer>> solucion= new ArrayList<>();
+
+    public KenKen(String nombre, int dificultad, String rutaImagen) {
+        this.nombre = nombre;
+        this.dificultad = dificultad;
+        this.rutaImagen = rutaImagen;
+    }
+
+
+    
+    
             
 }
