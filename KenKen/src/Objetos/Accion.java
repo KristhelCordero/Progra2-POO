47edcd74e289382113bba6005a24027a76fb300c
KendallPiedra <114965407+KendallPiedra@users.noreply.github.com>
2023-10-24 10:59:46 -0600

    fsd

--- KenKen/src/Objetos/Accion.java
+++ KenKen/src/Objetos/Accion.java
@@ -12,21 +12,54 @@ public class Accion {
     int fila;
     int columna;
     boolean agregar;//si se agrego un numero es true, si se quitó es false
-    int dato;
+    String dato;
 
-    public Accion(int fila, int columna, int dato) {
+    public Accion(int fila, int columna, String dato) {
         this.fila = fila;
         this.columna = columna;
-        this.agregar = false;
         this.dato = dato;
     }
     
+    
+    /*
     public Accion(int fila, int columna) {
         this.fila = fila;
         this.columna = columna;
         this.agregar = true;
     }
-    
+    */
+
+    public int getFila() {
+        return fila;
+    }
+
+    public void setFila(int fila) {
+        this.fila = fila;
+    }
+
+    public int getColumna() {
+        return columna;
+    }
+
+    public void setColumna(int columna) {
+        this.columna = columna;
+    }
+
+    public boolean isAgregar() {
+        return agregar;
+    }
+
+    public void setAgregar(boolean agregar) {
+        this.agregar = agregar;
+    }
+
+    public String getDato() {
+        return dato;
+    }
+
+    public void setDato(String dato) {
+        this.dato = dato;
+    }
     
     
     
