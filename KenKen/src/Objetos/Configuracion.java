bbf06868b4b4622f4277f78c91b20f9569ee588d
KendallPiedra <114965407+KendallPiedra@users.noreply.github.com>
2023-10-25 19:18:46 -0600

    gfdg

--- KenKen/src/Objetos/Configuracion.java
+++ KenKen/src/Objetos/Configuracion.java
@@ -21,7 +21,7 @@ public class Configuracion {
         this.timer.recetearTimer();
     }
 
-    Configuracion() {
+    public Configuracion() {
         this.posicionBarra = true;
         this.sonido = true;
         this.dificultad = 1;
@@ -60,6 +60,24 @@ public class Configuracion {
     public void setReloj(int reloj) {
         this.reloj = reloj;
     }
+
+    public boolean isPosicionBarra() {
+        return posicionBarra;
+    }
+
+    public void setPosicionBarra(boolean posicionBarra) {
+        this.posicionBarra = posicionBarra;
+    }
+
+    public Timer getTimer() {
+        return timer;
+    }
+
+    public void setTimer(Timer timer) {
+        this.timer = timer;
+    }
+    
+    
     
     public void setConfiguracion(boolean posicion, boolean sonido, int dificultad, int reloj) {
         this.posicionBarra = posicion;
@@ -70,4 +88,6 @@ public class Configuracion {
     public void resetear() {
         setConfiguracion(true,true,1,3);
     }
+    
+    
 }
