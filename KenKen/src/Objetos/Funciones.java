53b319212a1ff02e6622fd3342bd15b9f18291e3
KendallPiedra <114965407+KendallPiedra@users.noreply.github.com>
2023-10-30 18:59:26 -0600

    fdg

--- KenKen/src/Objetos/Funciones.java
+++ KenKen/src/Objetos/Funciones.java
@@ -71,12 +71,10 @@ public class Funciones {
         File archivo = new File("configuracion.dat");
         try{
             FileInputStream fis = new FileInputStream(archivo);
-            System.out.println("Lo encuentra");
             ObjectInputStream ois;
             ois = new ObjectInputStream(fis);
-            System.out.println("no se ha caido");
             Configuracion configuracion = (Configuracion) ois.readObject();
-            System.out.println("magia");
+            System.out.println("Archivo 'Configuracion.dat' leido con exito");
             return configuracion;
         }catch (Exception e){ 
             System.out.println("Error");
